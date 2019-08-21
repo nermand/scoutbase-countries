e3d744c5be6db98c2ebc1d260f40f7625fea29ca
nerman <nerman.deliahmetovic@gmail.com>
2019-08-21 16:42:27 +0200

    Move App.js into components folder

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import client from '../utils/graphql';
+import ErrorBoundary from './ErrorBoundary';
+import NavMenu from './NavMenu';
+import Routes from './Routes';
+
+import './App.css';
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <ErrorBoundary>
+          <NavMenu />
+          <Routes />
+        </ErrorBoundary>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
